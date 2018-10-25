2c278f8880144cf2d3f190ca50de31ac10b7e7c7
hieudauto <pmh.ars14@gmail.com>
2018-10-25 08:38:27 +0700

    modulized

--- messageSender.js
+++ messageSender.js
@@ -2,7 +2,7 @@ var request = require("request");
 
 const PAGE_ACCESS_TOKEN = 'EAAE73JnZByQcBAPEBSUGiRc8AldNfxVwxWE1Kajihe5dwlqlpUQKYZCGVfWl4RhQE19dZB26p163qPF0MYAir9x6mQ0ZAWkjUE9q6Ehq2Nky1QSS8fEuKsNd5oO4mqArPjp6F1aeZA9YaPQyGZAM3XR5YEvKJV9YSZBWW1ytBFYgu3QlcxWu1Uu';
  
-var sendMessage = (senderId, message) => {
+const sendMessage = (senderId, message) => {
 	request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: { access_token: PAGE_ACCESS_TOKEN, },
@@ -15,7 +15,7 @@ var sendMessage = (senderId, message) => {
     if (!error && response.statusCode === 200) {
       // Message has been successfully received by Facebook.
       console.log(
-        `Successfully sent message to ${endPoint} endpoint: `,
+        `Successfully sent message to endpoint: `,
         JSON.stringify(body)
       );
     } else {
@@ -29,4 +29,6 @@ var sendMessage = (senderId, message) => {
   });
 }
 
-module.exports = sendMessage;
+module.exports = {
+	sendMessage,
+};
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 var server = http.createServer(app);
 var request = require("request");
 
-// var sendMessage = require('../messageSender');
+var messageSender = require('./messageSender');
  
 app.get('/', (req, res) => {
   res.send("Home page. Server running okay.");
@@ -35,7 +35,7 @@ app.post('/webhook', function(req, res) { // Phần sử lý tin nhắn của ng
       entry.messaging.forEach(event => {
         if (event.message && event.message.text) {
           console.log("sent a message: " + event.message.text);
-          sendMessage(event.sender.id, "Hello I'm a bot replying to your message: '" 
+          messageSender.sendMessage(event.sender.id, "Hello I'm a bot replying to your message: '" 
             + event.message.text + "'");
         }
       });
@@ -48,32 +48,32 @@ app.post('/webhook', function(req, res) { // Phần sử lý tin nhắn của ng
 });
  
 // Đây là function dùng api của facebook để gửi tin nhắn
-function sendMessage(senderId, message) {
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN, },
-    method: 'POST',
-    json: {
-      recipient: { id: senderId },
-      message: { text: message },
-    }
-  }, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      // Message has been successfully received by Facebook.
-      console.log(
-        `Successfully sent message to endpoint: `,
-        JSON.stringify(body)
-      );
-    } else {
-      console.error(
-        `Failed calling Messenger API endpoint `,
-        response.statusCode,
-        response.statusMessage,
-        body.error
-      );
-    }
-  });
-}
+// function sendMessage(senderId, message) {
+//   request({
+//     url: 'https://graph.facebook.com/v2.6/me/messages',
+//     qs: { access_token: PAGE_ACCESS_TOKEN, },
+//     method: 'POST',
+//     json: {
+//       recipient: { id: senderId },
+//       message: { text: message },
+//     }
+//   }, (error, response, body) => {
+//     if (!error && response.statusCode === 200) {
+//       // Message has been successfully received by Facebook.
+//       console.log(
+//         `Successfully sent message to endpoint: `,
+//         JSON.stringify(body)
+//       );
+//     } else {
+//       console.error(
+//         `Failed calling Messenger API endpoint `,
+//         response.statusCode,
+//         response.statusMessage,
+//         body.error
+//       );
+//     }
+//   });
+// }
  
 app.set('port', process.env.PORT || 5000);
 app.set('ip', process.env.IP || "0.0.0.0");
