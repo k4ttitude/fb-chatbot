523d383eac7478fbf0b1811ccfa3672637618f0b
Minh Hiếu Phạm <pmh.ars14@gmail.com>
2018-11-13 05:03:15 +0700

    button slice 5

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.post('/webhook', function(req, res) { // Phần sử lý tin nhắn của ng
                     webview_height_ratio: 'full'
                 }
               });
-              messageSender.sendButtons(event.sender.id, "Select:", buttons);
+              messageSender.sendButtons(event.sender.id, "Select:", buttons.slice(0, 5));
               break;
             default:
               messageSender.sendMessage(event.sender.id, event.message.text);
